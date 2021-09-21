647d48a16d6feef63a84307c9279ab0379524d69
Victor <viktor.zubrinkin@visitech.ru>
2021-09-21 21:36:23 +0300

    feat(1-components): add shop-card

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,8 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICartProduct } from '../../../../../shared/mocks/1-components/cart-product';
 
 @Component({
 	selector: 'ngx-shop-card',
 	templateUrl: './shop-card.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShopCardComponent {}
+export class ShopCardComponent {
+	@Input()
+	public product: ICartProduct = {} as ICartProduct;
+
+	@Output()
+	public increment = new EventEmitter();
+
+	@Output()
+	public decrement = new EventEmitter();
+
+	public decrementProductInCart() {
+		this.decrement.emit();
+	}
+
+	public incrementProductInCart() {
+		this.increment.emit();
+	}
+}
