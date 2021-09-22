e7e7dccbc7ad9061b154c13716b33f0662f1d9fb
Victor <viktor.zubrinkin@visitech.ru>
2021-09-22 20:18:00 +0300

    feat(3-1): add categories

--- 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
+++ 3-directives/1-categories-menu-component/src/app/side-menu/side-menu.component.ts
@@ -1,8 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICategory } from '../../../../../shared/mocks/3-directives/categories';
 
 @Component({
 	selector: 'ngx-shop-side-menu',
 	templateUrl: './side-menu.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+	@Input()
+	public categories: ICategory[] = [];
+
+	@Output()
+	public categoryChange = new EventEmitter<string>();
+
+	public currentName: string | null = null;
+
+	public redirectTo(subCategory: string) {
+		this.categoryChange.emit(subCategory);
+	}
+
+	public hover(name: string) {
+		this.currentName = name;
+	}
+
+	public mouseLeave() {
+		this.currentName = null;
+	}
+}
