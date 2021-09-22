f6a7cdce368428a798a3daf025d6068b431269b1
Victor <viktor.zubrinkin@visitech.ru>
2021-09-22 09:51:11 +0300

    feat(1-3): add category product card

--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,8 +1,27 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+/* eslint-disable prettier/prettier */
+import {Component, ChangeDetectionStrategy, Input, Output, EventEmitter} from '@angular/core';
+import { IProduct } from '../../../../../shared/mocks/1-components/product';
 
 @Component({
 	selector: 'ngx-shop-content-product',
 	templateUrl: './product-card.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CategoryProductComponent {}
+export class CategoryProductComponent {
+	@Input()
+	public product: IProduct = {} as IProduct;
+
+	@Output()
+	public addToCart = new EventEmitter();
+
+	@Output()
+	public goToProduct = new EventEmitter();
+
+  public addToBasket() {
+    this.addToCart.emit();
+  }
+
+  public redirectTo() {
+    this.goToProduct.emit();
+  }
+}
