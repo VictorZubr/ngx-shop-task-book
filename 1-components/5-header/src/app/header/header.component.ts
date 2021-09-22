f5037ac3eacfb44b553ecc0a96cabc1b1986fc86
Victor <viktor.zubrinkin@visitech.ru>
2021-09-22 11:33:16 +0300

    feat(1-5): add header

--- 1-components/5-header/src/app/header/header.component.ts
+++ 1-components/5-header/src/app/header/header.component.ts
@@ -1,10 +1,17 @@
-import { Component, ChangeDetectionStrategy} from '@angular/core';
+import { Component, ChangeDetectionStrategy, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'ngx-shop-header',
-  templateUrl: './header.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'ngx-shop-header',
+	templateUrl: './header.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+	public counter = 5;
 
+	@Output()
+	public goToBasket = new EventEmitter();
+
+	public redirectTo() {
+		this.goToBasket.emit();
+	}
 }
